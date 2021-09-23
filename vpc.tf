bc54a16b90ed3506e63801259f3053b893783305
praveensounder <90690491+praveensounder@users.noreply.github.com>
2021-09-23 15:36:52 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -3,6 +3,30 @@ provider "aws" {
   access_key = "AKIA5G3MBGQF7GNPSVOO"
   secret_key = "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"
 }
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
 
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
