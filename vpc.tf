0dad596751028dee2b15e1fc476b673cfc5a30d7
praveensounder <90690491+praveensounder@users.noreply.github.com>
2021-09-23 05:15:14 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIA5G3MBGQF7GNPSVOO"
+  secret_key = "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"
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
